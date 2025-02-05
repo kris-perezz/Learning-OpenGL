eba01c4ce6e323edf038c3c26a76fd95566f3400
root <root@NITO-PC.>
2025-02-05 02:51:23 -0700

    trying again

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,32 @@ int main() {
     
     if (!glfwInit()) {
         std::cout << "GLFW couldn't start" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
     window = glfwCreateWindow(640, 480, "My Window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Couldn't load OpenGl" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+    glClearColor(0.5f, 0.1f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+    }
+
+    glfwTerminate();
 
     return 0;
 }
